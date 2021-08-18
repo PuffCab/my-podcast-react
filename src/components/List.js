65af52a7e0d0d07549c1a695a0cb80e761e17958
PuffCab <berlinkollner@gmail.com>
2021-08-18 13:33:00 +0200

    local fetch with json server added

--- src/components/List.js
+++ src/components/List.js
@@ -1,33 +1,51 @@
 import React, {useState, useEffect} from 'react'
 import { Paper, Button } from '@material-ui/core' 
 import ListItem from './ListItem'
+import { RestorePageRounded } from '@material-ui/icons'
 
 const List = () => {
     
     const [data, setData] = useState()
     
-    var myHeaders = new Headers();
-myHeaders.append("X-ListenAPI-Key", "3f0625c2f3eb4f8eae96a0293645a931");
+    //INICIO commented for local Fetch
+    // var myHeaders = new Headers();
+    // myHeaders.append("X-ListenAPI-Key", "3f0625c2f3eb4f8eae96a0293645a931");
 
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow' 
-};
+    // var requestOptions = {
+    //   method: 'GET',
+    //   headers: myHeaders,
+    //   redirect: 'follow' 
+    // };
+    
+    //    useEffect(() => {
+    //   const getData = async () => {
+        
+    //     const response = await fetch(
+    //       "https://listen-api.listennotes.com/api/v2/curated_podcasts/?page=*&", requestOptions
+    //     );
+    //     const obj = await response.json()
+    //     console.log(obj.curated_lists)
+    //     setData(obj.curated_lists)
+    //   };
+    //   getData();
+    // }, []);
+    //FIN commented for local Fetch
 
-     useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(
-        "https://listen-api.listennotes.com/api/v2/curated_podcasts/?page=*&", requestOptions
-      );
 
-      const obj = await response.json()
-      console.log(obj.curated_lists)
-      setData(obj.curated_lists)
+    useEffect(() => {
+      fetch("http://localhost:8000/obj")
+        .then(response => {
+          return response.json()
+        })
+        .then(obj => {
+          console.log(`obj`, obj)
+          setData(obj.curated_lists)
+        }); 
+
+      console.log(data)
+       
       
-    };
-    getData();
-  }, []);
+    }, [])
 
 
    
