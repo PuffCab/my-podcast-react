064bf1ead26aa4e52be734e1b87d8186a3b87fc4
PuffCab <berlinkollner@gmail.com>
2022-11-02 16:12:59 +0100

    id console.log removed

--- src/components/ListDetails.js
+++ src/components/ListDetails.js
@@ -1,47 +1,44 @@
-import React, { useState, useContext, useEffect} from 'react';
-import {
-  useParams 
-} from 'react-router-dom';
-import PodcastList from './PodcastList';
+import React, { useState, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import PodcastList from "./PodcastList";
 
 // import {PodcastsContext} from "../context/PodcastsContext"
 
-
-
-
 const ListDetails = () => {
-
   // const context = useContext(PodcastsContext)
   // const {curListDetails, getData} = useContext(PodcastsContext)
   // console.log(`context`, context)
 
-  const [curListDetails, setCurListDetails] = useState()
+  const [curListDetails, setCurListDetails] = useState();
 
-      let { id } = useParams()
-    let obj = useParams()
-    console.log(id)
-    // console.log(`object`, obj )
+  let { id } = useParams();
+  let obj = useParams();
+  // console.log(id)
+  // console.log(`object`, obj )
 
-    var myHeaders = new Headers();
-    myHeaders.append("X-ListenAPI-Key", process.env.REACT_APP_LISTENNOTES_API_KEY);
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "X-ListenAPI-Key",
+    process.env.REACT_APP_LISTENNOTES_API_KEY
+  );
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow' 
-    };
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
 
-     useEffect(() => {
+  useEffect(() => {
     const getCuratedList = async () => {
       const response = await fetch(
-        `https://listen-api.listennotes.com/api/v2/curated_podcasts/${id}`, requestOptions
+        `https://listen-api.listennotes.com/api/v2/curated_podcasts/${id}`,
+        requestOptions
       );
 
-      const obj = await response.json()
-    //   console.log(obj)
-      setCurListDetails(obj)
-    //   console.log(`response`, response)
-      
+      const obj = await response.json();
+      //   console.log(obj)
+      setCurListDetails(obj);
+      //   console.log(`response`, response)
     };
     getCuratedList();
     // getData();
@@ -49,28 +46,16 @@ const ListDetails = () => {
 
   // useEffect(() => {
   //   getData()
-    
+
   // }, [])
 
+  //   console.log(`curListDetails`, curListDetails)
 
-
-
-
-
- 
-//   console.log(`curListDetails`, curListDetails)
-
-    return (
-        
-        <div style={{marginTop: 40}}>
-             {
-                 curListDetails && <PodcastList curListDetails={curListDetails}/>
-             }
-             
-
-            
-        </div>
-    );
+  return (
+    <div style={{ marginTop: 40 }}>
+      {curListDetails && <PodcastList curListDetails={curListDetails} />}
+    </div>
+  );
 };
 
 export default ListDetails;
